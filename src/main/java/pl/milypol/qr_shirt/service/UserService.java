aa9bc635bc2037eeebc19644fdfc3429b5b8ded8
milypol98 <milypol98@gmail.com>
2022-08-23 11:25:34 +0200

    configuration

---
+++ src/main/java/pl/milypol/qr_shirt/service/UserService.java
@@ -0,0 +1,9 @@
+package pl.milypol.qr_shirt.service;
+
+import pl.milypol.qr_shirt.entity.User;
+
+public interface UserService {
+    User findByUserName(String name);
+
+    void saveUser(User user);
+}
