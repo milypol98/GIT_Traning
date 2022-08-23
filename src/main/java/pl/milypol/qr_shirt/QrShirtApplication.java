b936adf6e1ded19d29ac8de0ed1cfe90ddfcf6df
milypol98 <milypol98@gmail.com>
2022-08-23 11:22:13 +0200

    first commit

---
+++ src/main/java/pl/milypol/qr_shirt/QrShirtApplication.java
@@ -0,0 +1,13 @@
+package pl.milypol.qr_shirt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrShirtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrShirtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/milypol/qr_shirt/QrShirtApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milypol.qr_shirt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrShirtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
